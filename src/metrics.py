d3ffa80373aa8f27571ddfd6077f29dca5ff2d1d
viktorcikojevic <cikojevic.viktor@gmail.com>
2023-08-20 13:05:20 +0200

    added pr_auc and auroc metrics

--- src/metrics.py
+++ src/metrics.py
@@ -1,6 +1,6 @@
 import torch
-from sklearn.metrics import hamming_loss
-from sklearn.metrics import f1_score
+from sklearn.metrics import hamming_loss, average_precision_score, f1_score, roc_auc_score
+from sklearn.preprocessing import label_binarize
 
 def multi_label_accuracy(y_pred, targets, threshold=0.5, apply_sigmoid=True):
     """
@@ -48,3 +48,49 @@ def macro_f1_score_multilabel(y_pred, y_true, threshold=0.5, apply_sigmoid=True)
         y_pred_bin = (y_pred > threshold).float()
     
     return f1_score(y_true['y'].cpu().numpy(), y_pred_bin.cpu().numpy(), average='macro', zero_division=1)
+
+
+def pr_auc_multilabel(y_pred, y_true, apply_sigmoid=True):
+    """
+    Compute the macro averaged Precision-Recall Area Under Curve (PR AUC) for multilabel classification.
+    
+    y_pred: Tensor of shape [batch_size, n_classes] - Raw logits from the model
+    y_true: Tensor of shape [batch_size, n_classes] - True multilabels
+    """
+    n_classes = y_pred.size(1)
+    
+    # Convert logits to probabilities using sigmoid
+    if apply_sigmoid:
+        y_pred_prob = torch.sigmoid(y_pred).cpu().numpy()
+    else:
+        y_pred_prob = y_pred.cpu().numpy()
+    
+    y_true_bin = y_true['y'].cpu().numpy()
+    
+    # Compute PR AUC for each class and then average
+    average_precisions = []
+    for i in range(n_classes):
+        average_precisions.append(average_precision_score(y_true_bin[:, i], y_pred_prob[:, i]))
+    
+    # Return the macro average PR AUC
+    return sum(average_precisions) / n_classes
+
+
+
+def auroc_multilabel(y_pred, y_true, apply_sigmoid=True):
+    """
+    Compute the macro averaged Area Under the Receiver Operating Characteristic curve (AUROC) for multilabel classification.
+    
+    y_pred: Tensor of shape [batch_size, n_classes] - Raw logits from the model
+    y_true: Tensor of shape [batch_size, n_classes] - True multilabels
+    """
+    # Convert logits to probabilities using sigmoid
+    if apply_sigmoid:
+        y_pred_prob = torch.sigmoid(y_pred).cpu().numpy()
+    else:
+        y_pred_prob = y_pred.cpu().numpy()
+    
+    y_true_bin = y_true['y'].cpu().numpy()
+    
+    # Compute macro-average AUROC
+    return roc_auc_score(y_true_bin, y_pred_prob, average="macro")
